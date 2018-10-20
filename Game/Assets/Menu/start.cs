72814ddecc062b69281e52bcbfffa7b1d7a0c6b2
Dinkie <david.churchill72@gmail.com>
2018-10-20 12:34:53 +0100

    Menu buttons and more levels
    Hours awake: 29

--- Game/Assets/Menu/start.cs
+++ Game/Assets/Menu/start.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class start : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
-	void Update () {
-		
+	public void startGame() {
+		SceneManager.LoadScene("level1");
 	}
 }
